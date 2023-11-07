124a112aef2e35b112ecd88e304bf628cef94d4b
Victor Volkov <vektorn1212@gmail.com>
2023-11-07 14:04:19 +0200

    added models.py: added models for course and lesson

---
+++ materials/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    preview = models.ImageField(upload_to='course/', verbose_name='превью', **NULLABLE)
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    preview = models.ImageField(upload_to='lesson/', verbose_name='превью', **NULLABLE)
+    video_url = models.URLField(max_length=200, verbose_name='ссылка на видео', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
