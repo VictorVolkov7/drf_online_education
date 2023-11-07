864fe784be9cfcfb26165af095f6c500d563c85d
Victor Volkov <vektorn1212@gmail.com>
2023-11-07 14:06:09 +0200

    added urls.py: added urls for lessons (generics) and course (viewset)

---
+++ materials/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from materials.apps import MaterialsConfig
+from rest_framework import routers
+
+from materials.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
+
+app_name = MaterialsConfig.name
+
+router = routers.DefaultRouter()
+router.register(r'course', CourseViewSet, basename='course')
+
+urlpatterns = [
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-detail'),
+    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
+] + router.urls
