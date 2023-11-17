2cd07a8baf622fcddf881db31582e2c4b9d0038f
Victor Volkov <vektorn1212@gmail.com>
2023-11-17 14:24:29 +0200

    added permissions.py: added permissions for materials

---
+++ materials/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework.permissions import BasePermission
+
+from users.models import UserRole
+
+
+class IsModerator(BasePermission):
+    message = 'Вы не являетесь модератором!'
+
+    def has_permission(self, request, view):
+        return request.user.role == UserRole.MODERATOR
+
+
+class IsMaterialsOwner(BasePermission):
+    message = 'Вы не являетесь владельцем!'
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.pk == obj.pk
+
