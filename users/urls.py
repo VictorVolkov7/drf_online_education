c0b47cae80d8b221fc78bd8fca5ab0b40a939f0e
Victor Volkov <vektorn1212@gmail.com>
2023-11-17 14:22:33 +0200

    updated urls.py: added paths for tokens

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
-from users.apps import UsersConfig
+from django.urls import path
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
+from users.apps import UsersConfig
 from users.views import UserViewSet
 
 app_name = UsersConfig.name
@@ -9,5 +14,6 @@ router = routers.DefaultRouter()
 router.register(r'user', UserViewSet, basename='user')
 
 urlpatterns = [
-
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + router.urls
