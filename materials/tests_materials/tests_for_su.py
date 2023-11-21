f5b0af6ab2a9823dc3aed49e70c154580d91c0f9
Victor Volkov <vektorn1212@gmail.com>
2023-11-21 14:40:54 +0200

    added tests_for_su.py: added lesson tests for admin

---
+++ materials/tests_materials/tests_for_su.py
@@ -0,0 +1,200 @@
+from django.db import connection
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+
+from materials.models import Lesson
+from users.models import User
+
+
+class SULessonTestCase(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create(
+            email='admin@test.ru',
+            password='test',
+
+            is_staff=True,
+            is_active=True,
+            is_superuser=True,
+        )
+        self.client.force_authenticate(user=self.user)
+
+        self.lesson = Lesson.objects.create(
+            title='test',
+            description='test',
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Lesson.objects.all().delete()
+        super().tearDown()
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT setval(pg_get_serial_sequence('"users_user"','id'), 1, false);
+                SELECT setval(pg_get_serial_sequence('"materials_lesson"','id'), 1, false);
+            """)
+
+    def test_get_list(self):
+        """
+        Test for getting list of lessons for admin.
+        """
+
+        response = self.client.get(
+            reverse('materials:lesson-list')
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            response.json(),
+            {'count': 1, 'next': None, 'previous': None, 'results': [
+                {'id': 1, 'title': 'test', 'description': 'test', 'preview': None, 'video_url': None, 'course': None,
+                 'owner': None}]}
+        )
+
+    def test_lesson_create(self):
+        """
+        Test for creating lesson  for admin.
+        """
+        data = {
+            'title': 'test1',
+            'description': 'test1',
+        }
+
+        response = self.client.post(
+            reverse('materials:lesson-create'),
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+        self.assertEqual(
+            Lesson.objects.all().count(),
+            2
+        )
+
+        self.assertTrue(
+            response.json()['owner'] == self.user.pk
+        )
+
+    def test_lesson_retrieve(self):
+        """
+        Test for getting lesson details  for admin.
+        """
+
+        response = self.client.get(
+            reverse('materials:lesson-detail', kwargs={'pk': self.lesson.pk}),
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            response.json(),
+            {'id': self.lesson.pk, 'title': 'test', 'description': 'test', 'preview': None, 'video_url': None,
+             'course': None,
+             'owner': None}
+        )
+
+    def test_invalid_lesson_retrieve(self):
+        """
+        Test invalid for getting lesson details  for admin.
+        """
+
+        response = self.client.get(
+            reverse('materials:lesson-detail', kwargs={'pk': 999}),
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_404_NOT_FOUND
+        )
+
+    def test_lesson_update(self):
+        """
+        Test for updating lesson  for admin.
+        """
+
+        data = {
+            'title': 'test1',
+            'description': 'test1'
+        }
+
+        response = self.client.put(
+            reverse('materials:lesson-update', kwargs={'pk': self.lesson.pk}),
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            response.json(),
+            {'id': self.lesson.pk, 'title': 'test1', 'description': 'test1', 'preview': None, 'video_url': None,
+             'course': None, 'owner': None}
+        )
+
+    def test_invalid_lesson_update(self):
+        """
+        Test invalid for updating lesson  for admin.
+        """
+
+        data = {
+            'title': 'test999',
+            'description': 'test999'
+        }
+
+        response = self.client.put(
+            reverse('materials:lesson-update', kwargs={'pk': 999}),
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_404_NOT_FOUND
+        )
+
+    def test_lesson_delete(self):
+        """
+        Test for deleting lesson  for admin.
+        """
+
+        response = self.client.delete(
+            reverse('materials:lesson-delete', kwargs={'pk': self.lesson.pk})
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_204_NO_CONTENT
+        )
+
+        self.assertEqual(
+            Lesson.objects.all().count(),
+            0
+        )
+
+    def test_invalid_lesson_delete(self):
+        """
+        Test invalid for deleting lesson  for admin.
+        """
+
+        response = self.client.delete(
+            reverse('materials:lesson-delete', kwargs={'pk': 999})
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_404_NOT_FOUND
+        )
