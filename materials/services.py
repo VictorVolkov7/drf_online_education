f646312726761b30a25c181818ab256c91084d9d
Victor Volkov <vektorn1212@gmail.com>
2023-11-22 23:18:42 +0200

    updated services.py: hided secret key

--- materials/services.py
+++ materials/services.py
@@ -1,8 +1,10 @@
+import os
+
 import stripe
 
 
 def stripe_payment_created(material_name, material_price, user):
-    stripe.api_key = "sk_test_51OFIGVD1lQIcFlA4DX4qhcSPNY2v1M3VByHwMnWjsmIQOHNoqB43IoJrvf6HG1ly9LcfJkFOnnmP8K76dTL2s5aE00njByi0QQ"
+    stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
 
     product = stripe.Product.create(
         name=material_name,
